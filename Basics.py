28a7a90650817adb0841d2441fa10d758c8d3907
m1ckey <m1ckey@m1ckey.net>
2021-12-21 14:37:43 +0100

    edit text, fix minor math typos, format

--- Basics.py
+++ Basics.py
@@ -1,25 +1,27 @@
 import altair as alt
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
 import numpy as np
 import pandas as pd
 import streamlit as st
+from matplotlib.patches import Polygon
 
 np.random.seed(1)
 
-
-st.title('Basic concept of Monte Carlo Integration')
-st.write('The idea is to estimate a Integral of a function.')
-st.write('As simple example we use a univariate function in form:')
+st.title('Introduction to Monte Carlo Integration')
+st.write('The basic idea is to empirically estimate the definite integral of a function.')
+st.write('')
+st.write('We will use a univariate function as a simple example:')
+st.latex('f(x) = 2x')
 st.latex('F = \int_{a}^{b} f(x) \,dx')
 
-st.write('so let:')
-st.latex('F = \int_{0}^{1} 2x \,dx = 1')
+st.write('Geometric interpretation for $a=0$, $b=1$:')
+
 
 def f(x):
     return 2 * x
 
-def montecarlo(a,b,X,f):
+
+def montecarlo(a, b, X, f):
     '''
     :param a: lower limit
     :param b: upper limit
@@ -27,8 +29,8 @@ def montecarlo(a,b,X,f):
     :param f: function
     :return: estimate of the integral
     '''
-    y = f(X)
-    return (b-a)*y.sum()/len(X)
+    Y = f(X)
+    return (b - a) * Y.sum() / len(X)
 
 
 # integral limits
@@ -37,13 +39,13 @@ x = np.linspace(a, b)
 y = f(x)
 
 # plot function
-fig, ax = plt.subplots(figsize= (10,6))
+fig, ax = plt.subplots(figsize=(10, 6))
 ax.set_ylabel('f(x)')
 ax.set_xlabel('x')
 ax.grid(True)
-ax.axhline(y=0, color='k', linewidth = 0.5)
+ax.axhline(y=0, color='k', linewidth=0.5)
 
-ax.plot(x, y, 'r', linewidth=1)     # plot function line
+ax.plot(x, y, 'r', linewidth=1)  # plot function line
 
 # Make the shaded region
 ix = np.linspace(a, b)
@@ -51,58 +53,54 @@ iy = f(ix)
 verts = [(a, 0), *zip(ix, iy), (b, 0)]
 poly = Polygon(verts, facecolor='0.9', edgecolor='0.5')
 ax.add_patch(poly)
-ax.text(0.6 * (a + b), 0.3, r"$\int_0^1 f(x)\mathrm{d}x$",
+ax.text(0.6 * (a + b), 0.3, r"$\int_0^1 f(x)\mathrm{d}x = 1$",
         horizontalalignment='center', fontsize=20)
 
 st.pyplot(fig)  # plot figure
 
-st.write('We can take random points $x_i$ between $a=0$ and $b=1$. \
-We can get a rectangle area of each point by multiply $f(x_i)\cdot (b-a)$.')
-st.write('The idea of the Monte Carlo integration is to approximate the integrated value by sum up the rectangle areas and take the average of it.')
+st.write('If we take a random point $x_i$ between $a=0$ and $b=1$. '
+         r'We can construct the area of a rectangle by multiplying $f(x_i)\cdot (b-a)$.')
 
 # create subplots for each point
 x_rand = np.random.uniform(a, b, 9)
-fig, ax = plt.subplots(3,3, figsize=(10,6))
+fig, ax = plt.subplots(3, 3, figsize=(10, 6))
 for axs, x_i in zip(ax.reshape(-1), x_rand):
     axs.plot(x, y, 'r', linewidth=1)  # plot function line
-    verts = [(a, 0), (a, f(x_i)),(b, f(x_i)), (b, 0)]
+    verts = [(a, 0), (a, f(x_i)), (b, f(x_i)), (b, 0)]
     poly = Polygon(verts, facecolor='0.9', edgecolor='0.5')
     axs.add_patch(poly)
-    axs.plot(x_i,f(x_i),marker='o',color='b')
+    axs.plot(x_i, f(x_i), marker='o', color='b')
 st.pyplot(fig)  # plot figure
 
-st.write('Then the Monte Carlo estimator has the following form with $n \in \mathbb{N}$ for the random draws of $X$')
-st.latex(r'\left\langle F^n \right\rangle  = (b-a) \dfrac{1}{n-1} \sum_{i=0}^{n} f(X_i) ')
-st.write(f'for the upper example with 9 points we would get a estimate of $F={montecarlo(a,b,x_rand,f):.3f}$')
+st.write('The idea of Monte Carlo integration is to approximate the definite integral by drawing many $x_i$ from a '
+         '(uniform) distribution $X$ and taking the average of the areas.')
 st.write('')
-st.write('Not a good estimate because of few random numbers. But as we increase the numbers we will get a better result. \
-The law of large numbers gives us that as $n$ approaches infinity, the the Monte Carlo estimator converges in probability to $F$')
+st.write('The Monte Carlo estimator has the following form with $n$ random draws from $X$.')
+st.latex(r'\left\langle F^n \right\rangle  = \dfrac{(b-a)}{n} \sum_{i=1}^{n} f(X_i)')
+st.write(f'for the upper example with $n=9$ we get an estimate of $F={montecarlo(a, b, x_rand, f):.3f}$')
+st.write('')
+st.write('Not a good estimate because $n$ is small. But as we increase $n$ we will get a '
+         'better result. The law of large numbers says that the the Monte Carlo estimator '
+         'converges in probability to $F$')
 st.latex(r' Pr\left\lbrace \lim\limits_{n\to \infty} \left\langle F^n\right\rangle  = F \right\rbrace = 1')
 
 x_rand = np.random.uniform(a, b, 1000)
-st.write(f'If we increase the random numbers, for example 1000 points we get a estimate of $F={montecarlo(a,b,x_rand,f):.3f}$ , a much better result')
+st.write(f'If we set $n=1000$ we get an estimate of $F={montecarlo(a, b, x_rand, f):.3f}$ , a much better result')
 
+# start of interactive example
+st.header('Interactive Example')
 
-# start of Interactive Code
-st.header('Interactive code')
-
-n_max = st.number_input('Maximum Random Numbers', min_value=1, step=1, value=1000)
-a = st.number_input('Lower Bound (a)', value=0.0, step=0.1)
-b = st.number_input('Upper Bound (b)', value=1.1, step=0.1)
+a = st.number_input('Lower Bound (a)', value=0., step=0.1)
+b = st.number_input('Upper Bound (b)', value=1., step=0.1, min_value=a)
+n_max = int(st.number_input('Max number of samples', min_value=1, step=1, value=2000))
+n = int(st.slider('Number of samples (n)', min_value=1, max_value=n_max, step=1, value=10))
 
 X_all = np.random.uniform(a, b, n_max)
-
-n = st.slider('Number of samples (n)',
-              min_value=1,
-              max_value=n_max,
-              step=1,
-              value=10)
 X = X_all[:n]
 Y = f(X)
 
 # result montecarlo
-st.write(f'$F^n={montecarlo(a,b,X,f):.3f}$')
-
+st.write(f'$F^n={montecarlo(a, b, X, f):.3f}$')
 
 source = pd.DataFrame({'x': X, 'y': Y})
 chart = alt.Chart(source)
@@ -122,7 +120,7 @@ st.altair_chart(samples + samples_mean)
 N = np.arange(n) + 1
 A = np.zeros_like(X)
 for i in range(n):
-    A[i] = montecarlo(a,b,X[:i + 1],f)
+    A[i] = montecarlo(a, b, X[:i + 1], f)
 
 source = pd.DataFrame({'Number of samples': N, 'F(x)': A})
 chart = alt.Chart(source).mark_line(color='violet').encode(
