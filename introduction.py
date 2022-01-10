d579bd5c36d24b74f77c750ad7760b1665ab309b
Richi0D <46872517+Richi0D@users.noreply.github.com>
2022-01-10 21:24:14 +0100

    add input for any function as user input

--- introduction.py
+++ introduction.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from matplotlib.patches import Polygon
+from scipy import stats
 
 np.random.seed(1)
 
@@ -76,44 +77,66 @@ st.write('The idea of Monte Carlo integration is to approximate the definite int
          '(uniform) distribution $X$ and taking the average of the areas.')
 st.write('')
 st.write('The Monte Carlo estimator has the following form with $n$ random draws from $X$.')
-st.latex(r'\left\langle F^n \right\rangle  = \dfrac{(b-a)}{n} \sum_{i=1}^{n} f(X_i)')
+st.latex(r'I  \approx \dfrac{(b-a)}{n} \sum_{i=1}^{n} f(X_i)')
 st.write(f'for the upper example with $n=9$ we get an estimate of $F={montecarlo(a, b, x_rand, f):.3f}$')
 st.write('')
 st.write('Not a good estimate because $n$ is small. But as we increase $n$ we will get a '
          'better result. The law of large numbers says that the the Monte Carlo estimator '
          'converges in probability to $F$')
-st.latex(r' Pr\left\lbrace \lim\limits_{n\to \infty} \left\langle F^n\right\rangle  = F \right\rbrace = 1')
+st.latex(r' Pr\left\lbrace \lim\limits_{n\to \infty} I  = F \right\rbrace = 1')
 
 x_rand = np.random.uniform(a, b, 1000)
 st.write(f'If we set $n=1000$ we get an estimate of $F={montecarlo(a, b, x_rand, f):.3f}$ , a much better result')
 
-# start of interactive example
-st.header('Interactive Example')
 
+# start of interactive example
+st.header('1D Interactive Example')
+
+
+f_user = st.text_input('f(x): function as python code!'
+                       ' [Addition: + , Subtraction: -, Multiplication: *, Division: /, Power: **]', value='2*x')
 a = st.number_input('Lower Bound (a)', value=0., step=0.1)
 b = st.number_input('Upper Bound (b)', value=1., step=0.1, min_value=a)
 n_max = int(st.number_input('Max number of samples', min_value=1, step=1, value=2000))
 n = int(st.slider('Number of samples (n)', min_value=1, max_value=n_max, step=1, value=10))
 
+
+# create function from string
+#f_u = "f=lambda x :"
+f_u = f"""
+def userfun(x):
+    return {f_user}
+"""
+cc = f_u
+exec(cc)
+
+test = userfun(3)
+
+
 X_all = np.random.uniform(a, b, n_max)
 X = X_all[:n]
-Y = f(X)
+Y = userfun(X)
 
 # result montecarlo
-st.write(f'$F^n={montecarlo(a, b, X, f):.3f}$')
+st.subheader('1D Crude Monte Carlo method')
+st.write(f'$I={montecarlo(a, b, X, f):.3f}$')
 
 source = pd.DataFrame({'x': X, 'f(x)': Y})
 chart = alt.Chart(source)
 samples = chart.mark_point().encode(
     x=alt.X('x', scale=alt.Scale(domain=[a, b])),
-    y=alt.Y('f(x)', scale=alt.Scale(domain=[f(a), f(b)]), title='f(x)'),
+    y=alt.Y('f(x)', scale=alt.Scale(domain=[min(Y), max(Y)]), title='f(x)'),
+).properties(
+    title='Samples'
 )
+
 samples_mean = chart.mark_rule(
     color='violet',
     tooltip='arithmetic mean of f(x)'
 ).encode(
     y=alt.Y('average(f(x))', title='mean f(x)')
 )
+
 st.altair_chart(samples + samples_mean)
 
 N = np.arange(n) + 1
@@ -121,9 +144,11 @@ A = np.zeros_like(X)
 for i in range(n):
     A[i] = montecarlo(a, b, X[:i + 1], f)
 
-source = pd.DataFrame({'n': N, 'mean f(x)': A})
+source = pd.DataFrame({'n': N, 'I': A})
 chart = alt.Chart(source).mark_line(color='violet').encode(
     x='n',
-    y=alt.Y('mean f(x)', scale=alt.Scale(domain=[source.min()[1], source.max()[1]])),
+    y=alt.Y('I', scale=alt.Scale(domain=[source.min()[1], source.max()[1]]), title='Approximation of F(x)'),
+).properties(
+    title='Montecarlo'
 )
 st.altair_chart(chart)
